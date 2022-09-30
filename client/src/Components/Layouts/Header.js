079290d923e8797d4cb56762718f2a19f81f58d6
Esteban Olivera <estolwebdesign@outlook.com>
2022-09-30 19:59:25 -0300

    fix navbar collapse

--- client/src/Components/Layouts/Header.js
+++ client/src/Components/Layouts/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useLayoutEffect, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import UserContext from "../../Context/UserContext";
 import { API } from "../../Global";
@@ -7,6 +7,9 @@ import logo from "../../chromeswap-logo.png";
 
 const Header = () => {
   const { user, setUser } = useContext(UserContext);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [isCollapsing, setIsCollapsing] = useState(false);
+  const [winWidth, setWinWidth] = useState(0);
 
   const navigate = useNavigate();
 
@@ -31,21 +34,47 @@ const Header = () => {
       });
   };
 
+  const handleNavbarToggle = () => {
+    setIsCollapsing(true);
+    setTimeout(() => {
+      setIsCollapsing(false);
+      setIsCollapsed(!isCollapsed);
+    }, 350);
+  };
+
+  useLayoutEffect(() => {
+    function updateWidth() {
+      setWinWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", updateWidth);
+    updateWidth();
+    return () => window.removeEventListener("resize", updateWidth);
+  }, [winWidth]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-qatar fixed-top" style={{ zIndex: "1100" }}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img src={logo} alt="chromeswap" height="60px" />
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={handleNavbarToggle}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={winWidth > 991 ? "navbar-collapse collapse" : `navbar-collapse ${isCollapsed === false ? "show" : ""} ${isCollapsing === true ? "collapsing" : "collapse"}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             {!user ? (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/sign">
+                  <NavLink className="nav-link" to="/sign" onClick={handleNavbarToggle}>
                     Ingresar
                   </NavLink>
                 </li>
@@ -53,29 +82,36 @@ const Header = () => {
             ) : (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/search-chromes">
+                  <NavLink className="nav-link" to="/search-chromes" onClick={handleNavbarToggle}>
                     Buscar figuritas
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/my-chromes">
+                  <NavLink className="nav-link" to="/my-chromes" onClick={handleNavbarToggle}>
                     Mis figuritas
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/my-transactions">
+                  <NavLink className="nav-link" to="/my-transactions" onClick={handleNavbarToggle}>
                     Mis intercambios
                   </NavLink>
                 </li>
                 {user.roles[0] === "ROLE_ADMIN" && (
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="admin">
+                    <NavLink className="nav-link" to="admin" onClick={handleNavbarToggle}>
                       Administrar
                     </NavLink>
                   </li>
                 )}
                 <li className="nav-item">
-                  <span role="button" className="nav-link" onClick={handleSignOut}>
+                  <span
+                    role="button"
+                    className="nav-link"
+                    onClick={() => {
+                      handleSignOut();
+                      handleNavbarToggle();
+                    }}
+                  >
                     Cerrar sesión
                   </span>
                 </li>
