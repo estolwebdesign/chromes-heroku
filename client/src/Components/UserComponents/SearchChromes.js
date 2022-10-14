7576666496d5390908076d4b3030ba22c9e781a2
Esteban Olivera <estol1991@gmail.com>
2022-10-14 19:57:44 -0300

    update searcjChromes component & build folder

--- client/src/Components/UserComponents/SearchChromes.js
+++ client/src/Components/UserComponents/SearchChromes.js
@@ -26,7 +26,6 @@ const SearchChromes = () => {
 
   useEffect(() => {
     if (latitude && longitude && user.id) {
-      
       const body = {
         lat: latitude.toString(),
         lng: longitude.toString(),
@@ -35,7 +34,7 @@ const SearchChromes = () => {
       const requestOptions = {
         method: "POST",
         credentials: "include",
-        headers: { "Content-Type": "application/json" },        
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       };
 
@@ -89,8 +88,11 @@ const SearchChromes = () => {
   };
 
   useEffect(() => {
-    if (users?.length > 0 && !sorted) {
+    if (users?.length > 1 && !sorted) {
       users.sort((a, b) => (a.distance > b.distance ? 1 : -1));
+      setSorted(true);
+    }
+    if (users?.length === 1) {
       setSorted(true);
     }
 
@@ -101,8 +103,14 @@ const SearchChromes = () => {
     <main className="mb-5">
       <div className="container bg-light rounded shadow p-3 mt-3">
         <div className="d-flex flex-column position-relative w-100">
-          <hr className="text-light mt-4 w-100 position-absolute" style={{ borderTop: "4px solid #5c0931" }} />
-          <h1 className="text-center bg-qatar px-5 rounded-pill mx-auto fw-bold text-qatar text-light" style={{ zIndex: "1050" }}>
+          <hr
+            className="text-light mt-4 w-100 position-absolute"
+            style={{ borderTop: "4px solid #5c0931" }}
+          />
+          <h1
+            className="text-center bg-qatar px-5 rounded-pill mx-auto fw-bold text-qatar text-light"
+            style={{ zIndex: "1050" }}
+          >
             FIGURITAS CERCANAS
           </h1>
           {!users ? (
@@ -112,33 +120,61 @@ const SearchChromes = () => {
               {users.length < 1 ? (
                 <div className="mt-3">
                   <p className="fs-3 mb-0 text-center fw-bold">Lo sentimos</p>
-                  <p className="fs-3 mb-0 text-center">No se encontraron usuarios cercanos</p>
+                  <p className="fs-3 mb-0 text-center">
+                    No se encontraron usuarios cercanos
+                  </p>
                 </div>
               ) : (
                 <div>
                   {sorted &&
                     users.map((usr, i) => (
-                      <section key={i} id={usr._id} className="border rounded p-3 mb-3 shadow">
+                      <section
+                        key={i}
+                        id={usr._id}
+                        className="border rounded p-3 mb-3 shadow"
+                      >
                         <div id="user-header" className="mb-2">
                           <div className="row">
                             <div className="col-lg-4 order-lg-0 order-2 d-flex">
                               {usr.rating ? (
                                 <div className="m-auto d-flex">
-                                  <span className="my-auto me-2">Valoración: </span>
-                                  {[...Array(parseInt((usr.rating).toFixed(1).split(".")[0]))].map((star, index) => {
+                                  <span className="my-auto me-2">
+                                    Valoración:{" "}
+                                  </span>
+                                  {[
+                                    ...Array(
+                                      parseInt(
+                                        usr.rating.toFixed(1).split(".")[0]
+                                      )
+                                    ),
+                                  ].map((star, index) => {
                                     index += 1;
                                     return (
-                                      <span key={index} className="star on fs-2" style={{ textShadow: "none" }}>
+                                      <span
+                                        key={index}
+                                        className="star on fs-2"
+                                        style={{ textShadow: "none" }}
+                                      >
                                         &#9733;
                                       </span>
                                     );
                                   })}
-                                  {parseInt((usr.rating).toFixed(1).split(".")[1]) > 0 && (
+                                  {parseInt(
+                                    usr.rating.toFixed(1).split(".")[1]
+                                  ) > 0 && (
                                     <span
                                       className="star fs-2"
                                       style={{
-                                        backgroundImage: `linear-gradient(90deg, #ffd000 ${parseInt(usr.rating?.toString().split(".")[1]) * 10}%, #b9b9b9 ${
-                                          100 - parseInt(usr.rating?.toString().split(".")[1]) * 10
+                                        backgroundImage: `linear-gradient(90deg, #ffd000 ${
+                                          parseInt(
+                                            usr.rating?.toString().split(".")[1]
+                                          ) * 10
+                                        }%, #b9b9b9 ${
+                                          100 -
+                                          parseInt(
+                                            usr.rating?.toString().split(".")[1]
+                                          ) *
+                                            10
                                         }%)`,
                                         color: "transparent",
                                         WebkitBackgroundClip: "text",
@@ -149,32 +185,67 @@ const SearchChromes = () => {
                                       &#9733;
                                     </span>
                                   )}
-                                  {parseInt((usr.rating).toFixed(1).split(".")[0]) < 4 &&
-                                    [...Array(4 - parseInt(usr.rating.toString().split(".")[0]))].map((star, index) => {
+                                  {parseInt(
+                                    usr.rating.toFixed(1).split(".")[0]
+                                  ) < 4 &&
+                                    [
+                                      ...Array(
+                                        4 -
+                                          parseInt(
+                                            usr.rating.toString().split(".")[0]
+                                          )
+                                      ),
+                                    ].map((star, index) => {
                                       index += 1;
                                       return (
-                                        <span key={index} className="star off fs-2" style={{ textShadow: "none" }}>
+                                        <span
+                                          key={index}
+                                          className="star off fs-2"
+                                          style={{ textShadow: "none" }}
+                                        >
                                           &#9733;
                                         </span>
                                       );
                                     })}
-                                  {parseInt((usr.rating).toFixed(1).split(".")[0]) < 5 && parseInt((usr.rating).toFixed(1).split(".")[1]) === 0 && (
-                                    <span className="star off fs-2" style={{ textShadow: "none" }}>
-                                      &#9733;
-                                    </span>
-                                  )}
+                                  {parseInt(
+                                    usr.rating.toFixed(1).split(".")[0]
+                                  ) < 5 &&
+                                    parseInt(
+                                      usr.rating.toFixed(1).split(".")[1]
+                                    ) === 0 && (
+                                      <span
+                                        className="star off fs-2"
+                                        style={{ textShadow: "none" }}
+                                      >
+                                        &#9733;
+                                      </span>
+                                    )}
                                 </div>
                               ) : (
-                                <h6 className="m-auto">No tiene valoraciones aún</h6>
+                                <h6 className="m-auto">
+                                  No tiene valoraciones aún
+                                </h6>
                               )}
                             </div>
                             <div className="col-lg-4 order-lg-1 order-1 d-flex">
-                              <h2 className="fw-bold m-auto text-qatar">{usr.username.split(/\s+/)[0].toUpperCase()}</h2>
+                              <h2 className="fw-bold m-auto text-qatar">
+                                {usr.username.split(/\s+/)[0].toUpperCase()}
+                              </h2>
                             </div>
                             <div className="col-lg-4 order-lg-2 order-0 d-flex">
-                              <div className="ms-lg-auto mx-auto my-auto" style={{ height: "24px" }}>
-                                <span style={{ height: "24px" }} className="ms-auto">
-                                  Distancia: {usr.distance < 1000 ? "Menos de 1 Km" : `${usr.distance / 1000} Kms`} <FontAwesomeIcon icon={faLocationPin} />
+                              <div
+                                className="ms-lg-auto mx-auto my-auto"
+                                style={{ height: "24px" }}
+                              >
+                                <span
+                                  style={{ height: "24px" }}
+                                  className="ms-auto"
+                                >
+                                  Distancia:{" "}
+                                  {usr.distance < 1000
+                                    ? "Menos de 1 Km"
+                                    : `${usr.distance / 1000} Kms`}{" "}
+                                  <FontAwesomeIcon icon={faLocationPin} />
                                 </span>
                               </div>
                             </div>
@@ -184,20 +255,34 @@ const SearchChromes = () => {
                           {usr.repeated.map((repeat, i) => {
                             return (
                               !user?.chromes.includes(repeat.chrome._id) && (
-                                <div key={i} className="col-lg-2 col-md-3 col-sm-4 col-6">
+                                <div
+                                  key={i}
+                                  className="col-lg-2 col-md-3 col-sm-4 col-6"
+                                >
                                   <article
-                                    className={`bg-secondary mx-auto rounded shadow ${user?.chromes.includes(repeat.chrome._id) ? "bg-danger" : "bg-success"}`}
+                                    className={`bg-secondary mx-auto rounded shadow ${
+                                      user?.chromes.includes(repeat.chrome._id)
+                                        ? "bg-danger"
+                                        : "bg-success"
+                                    }`}
                                     style={{ height: "200px", width: "133px" }}
                                   >
                                     <div className="d-flex flex-column h-100">
-                                      <div className="fs-3 text-light fw-bold m-auto">{repeat.chrome.name}</div>
+                                      <div className="fs-3 text-light fw-bold m-auto">
+                                        {repeat.chrome.name}
+                                      </div>
                                       <form
                                         onSubmit={(e) => {
                                           e.preventDefault();
-                                          handleNewTransaction(repeat.chrome._id, usr._id);
+                                          handleNewTransaction(
+                                            repeat.chrome._id,
+                                            usr._id
+                                          );
                                         }}
                                       >
-                                        <button className="btn btn-qatar w-100">Intercambiar</button>
+                                        <button className="btn btn-qatar w-100">
+                                          Intercambiar
+                                        </button>
                                       </form>
                                     </div>
                                   </article>
